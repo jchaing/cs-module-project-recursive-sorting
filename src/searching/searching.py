631773a39014fdb2fb3cea20f99dfb285161e895
john <john@spinningplates.org>
2020-08-09 21:20:03 -0700

    Completed searching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,13 +2,44 @@
 def binary_search(arr, target, start, end):
     # Your code here
 
+    # Check if start is greater than end
+    if start > end:
+        return -1
+
+    else:
+        # store middle index
+        middle = (start + end) // 2
+
+        # store guess
+        guess = arr[middle]
+
+        # if guess is the target, return the index
+        if guess == target:
+            print(guess)
+            return middle
+
+        # if target is greater than guess, recursive search right half
+        elif target > guess:
+            return binary_search(arr, target, (middle + 1), end)
+
+        # if target is less than guess, recursive search left half
+        else:
+            return binary_search(arr, target, start, (middle - 1))
+
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
+
+
 def agnostic_binary_search(arr, target):
     # Your code here
+    pass
+
+
+arr = [1, 2, 3, 4, 5]
+binary_search(arr, 3, 0, len(arr) - 1)
 
